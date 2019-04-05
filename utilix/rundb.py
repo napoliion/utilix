e88f07cb58d7dfff505990a82947bfbea50bedc9
Boris <Boris.Bauermeister@fysik.su.se>
2019-04-05 17:16:22 +0200

    add http request codes for decoding

--- utilix/rundb.py
+++ utilix/rundb.py
@@ -6,12 +6,108 @@ import logging
 from utilix.config import Config
 
 logger = logging.getLogger("utilix")
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 config = Config()
+
 
 PREFIX = config.get('RunDB', 'rundb_api_url')
 BASE_HEADERS = {'Content-Type': "application/json", 'Cache-Control': "no-cache"}
 
+def Responder(func):
+
+    def LookUp():
+        return_dict = {
+            #taken from https://github.com/kennethreitz/requests/blob/master/requests/status_codes.py
+            # Informational.
+            100: ('continue',),
+            101: ('switching_protocols',),
+            102: ('processing',),
+            103: ('checkpoint',),
+            122: ('uri_too_long', 'request_uri_too_long'),
+            200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
+            201: ('created',),
+            202: ('accepted',),
+            203: ('non_authoritative_info', 'non_authoritative_information'),
+            204: ('no_content',),
+            205: ('reset_content', 'reset'),
+            206: ('partial_content', 'partial'),
+            207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
+            208: ('already_reported',),
+            226: ('im_used',),
+
+            # Redirection.
+            300: ('multiple_choices',),
+            301: ('moved_permanently', 'moved', '\\o-'),
+            302: ('found',),
+            303: ('see_other', 'other'),
+            304: ('not_modified',),
+            305: ('use_proxy',),
+            306: ('switch_proxy',),
+            307: ('temporary_redirect', 'temporary_moved', 'temporary'),
+            308: ('permanent_redirect',
+                  'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
+
+            # Client Error.
+            400: ('bad_request', 'bad'),
+            401: ('unauthorized',),
+            402: ('payment_required', 'payment'),
+            403: ('forbidden',),
+            404: ('not_found', '-o-'),
+            405: ('method_not_allowed', 'not_allowed'),
+            406: ('not_acceptable',),
+            407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
+            408: ('request_timeout', 'timeout'),
+            409: ('conflict',),
+            410: ('gone',),
+            411: ('length_required',),
+            412: ('precondition_failed', 'precondition'),
+            413: ('request_entity_too_large',),
+            414: ('request_uri_too_large',),
+            415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
+            416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
+            417: ('expectation_failed',),
+            418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
+            421: ('misdirected_request',),
+            422: ('unprocessable_entity', 'unprocessable'),
+            423: ('locked',),
+            424: ('failed_dependency', 'dependency'),
+            425: ('unordered_collection', 'unordered'),
+            426: ('upgrade_required', 'upgrade'),
+            428: ('precondition_required', 'precondition'),
+            429: ('too_many_requests', 'too_many'),
+            431: ('header_fields_too_large', 'fields_too_large'),
+            444: ('no_response', 'none'),
+            449: ('retry_with', 'retry'),
+            450: ('blocked_by_windows_parental_controls', 'parental_controls'),
+            451: ('unavailable_for_legal_reasons', 'legal_reasons'),
+            499: ('client_closed_request',),
+
+            # Server Error.
+            500: ('internal_server_error', 'server_error', '/o\\', '✗'),
+            501: ('not_implemented',),
+            502: ('bad_gateway',),
+            503: ('service_unavailable', 'unavailable'),
+            504: ('gateway_timeout',),
+            505: ('http_version_not_supported', 'http_version'),
+            506: ('variant_also_negotiates',),
+            507: ('insufficient_storage',),
+            509: ('bandwidth_limit_exceeded', 'bandwidth'),
+            510: ('not_extended',),
+            511: ('network_authentication_required', 'network_auth', 'network_authentication'),
+            }
+        return return_dict
+
+    def func_wrapper(*args, **kwargs):
+        st = func(*args, **kwargs)
+        if st.status_code != 200:
+            logger.error("HTTP(s) request says: {0} (Code {1})".format(LookUp()[st.status_code][0], st.status_code))
+        return st
+    return func_wrapper
 
 class Token:
     """
@@ -112,15 +208,19 @@ class DB:
         self._setup()
 
     #Helper:
+    @Responder
     def _get(self, url):
         return requests.get(PREFIX + url, headers=self.headers)
 
+    @Responder
     def _put(self, url, data):
         return requests.put(PREFIX + url, data, headers=self.headers)
 
+    @Responder
     def _post(self, url, data):
         return requests.post(PREFIX + url, data, headers=self.headers)
 
+    @Responder
     def _delete(self, url, data):
         return requests.delete(PREFIX + url, data=data, headers=self.headers)
 
@@ -163,16 +263,15 @@ class DB:
         url = '/runs/{selector}/{num}/data/dids'.format(selector=self.selector, num=self.select)
         return json.loads(self._get(url).text)['results']['dids']
 
-    def update_data(self, datum):
-        datum = json.dump(datum)
-        print(datum)
+    def update_data(self, data_field):
+        data_field = json.dumps(data_field)
         url = '/run/{selector}/{num}/data/'.format(selector=self.selector, num=self.select)
-        return self._post(url, data=datum)
+        return self._post(url, data=data_field)
 
-    def delete_datum(self, datum):
-        datum = json.dump(datum)
+    def delete_data(self, data_field):
+        data_field = json.dumps(data_field)
         url = '/run/{selector}/{num}/data/'.format(selector=self.selector, num=self.select)
-        return self._delete(url, data=datum)
+        return self._delete(url, data=data_field)
 
 
 # for testing
