248e75ec1f8b72dd8408472d7282df9d75a9588f
Evan Shockley <ershockley@uchicago.edu>
2020-07-29 18:02:49 -0500

    Add try except to __init__

--- utilix/__init__.py
+++ utilix/__init__.py
@@ -1,4 +1,8 @@
-from utilix.rundb import DB
+try:
+    from utilix.rundb import DB
 
-# instantiate here so we just do it once
-db = DB()
+    # instantiate here so we just do it once
+    db = DB()
+
+except:
+    print("Error initializing RunDB. Maybe no ~/.xenon_config file?")
