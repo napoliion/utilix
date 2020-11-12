94ce6d9b5d149c20d64aa24a1a974dc3fba9bccd
Evan Shockley <ershockley@uchicago.edu>
2020-11-12 13:43:01 -0600

    Fix handling of config paths

--- utilix/config.py
+++ utilix/config.py
@@ -20,14 +20,14 @@ class Config():
 
     def __init__(self, path=None):
         if not Config.instance:
-            Config.instance = Config.__Config(path=path)
+            Config.instance = Config.__Config()
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
 
     class __Config(configparser.ConfigParser):
 
-        def __init__(self, path=None):
+        def __init__(self):
 
             if 'XENON_CONFIG' not in os.environ:
                 logger.warning('$XENON_CONFIG is not defined in the environment')
@@ -36,17 +36,18 @@ class Config():
             home_config = os.path.join(os.environ['HOME'], '.xenon_config')
             xenon_config = os.environ.get('XENON_CONFIG')
 
-            # if path is passed, use that
-            if path:
-                config_file_path = path
-
             # if not, see if there is a XENON_CONFIG environment variable
-            elif xenon_config:
+            if xenon_config:
                 config_file_path = os.environ.get('XENON_CONFIG')
 
             # if not, then look for hidden file in HOME
-            elif os.path.exists(os.path.join(os.environ['HOME'], '.xenon_config')):
+            elif os.path.exists(home_config):
                 config_file_path = home_config
+
+            else:
+                raise FileNotFoundError(f"Could not load a configuration file. "
+                                        f"You can create one at {home_config}, or set a custom path using\n\n"
+                                        f"export XENON_CONFIG={{path_to_your_config}}")
 
             logger.debug('Loading configuration from %s' % (config_file_path))
             configparser.ConfigParser.__init__(self, interpolation=EnvInterpolation())
