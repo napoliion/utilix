6a556b0429d2133d7888c28fe34203111b59c5ff
Joran Angevaare <jorana@nikhef.nl>
2020-11-25 18:27:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup, find_packages
 
+# Get requirements from requirements.txt, stripping the version tags
+with open('requirements.txt') as f:
+    requires = [
+        r.split('/')[-1] if r.startswith('git+') else r
+        for r in f.read().splitlines()]
+
+with open('README.md') as file:
+    readme = file.read()
+
 setup(
     name = "utilix",
     version = "0.2.0",
     url='https://github.com/XENONnT/utilix',
     description ="User-friendly interface to various utilities for XENON users",
-    packages=find_packages()
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=requires,
+    python_requires=">=3.6",
 )
