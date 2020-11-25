a902cbdf089149f591da93c28b7327cfefcd54b4
Evan Shockley <ershockley@uchicago.edu>
2020-11-25 09:17:20 -0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "utilix",
-    version = "0.1.0",
+    version = "0.2.0",
     url='https://github.com/XENONnT/utilix',
     description ="User-friendly interface to various utilities for XENON users",
     packages=find_packages()
